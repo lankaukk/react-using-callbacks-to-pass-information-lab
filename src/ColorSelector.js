14468b920ca15f213732abe898ed45977559f554
McKayla Lankau <lankaukk@yahoo.com>
2021-09-09 13:16:15 -0400

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,7 +4,13 @@ export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      const cb = () => { this.props.setSelectedColor(str) 
+        
+        const palette = document.getElementById('colorSelector')
+        console.log(palette)
+        palette.style.backgroundColor = str
+      }
+      return <div key={idx} onClick={cb} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
 
@@ -16,3 +22,4 @@ export default class ColorSelector extends Component {
     )
   }
 }
+
